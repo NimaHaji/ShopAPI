f2238c2a40951b4c5c182ce96381e78c5bcefa44
Nima Haji <nimahaji1395@gmail.com>
2026-07-31 18:10:31 +0330

    Order Controller added some action

--- Application/Features/Order/Interfaces/OrderRepositoryContract.cs
+++ Application/Features/Order/Interfaces/OrderRepositoryContract.cs
@@ -2,8 +2,10 @@ namespace Application.Features.Order.Interfaces;
 
 public interface OrderRepositoryContract
 {
-    Task<Domain.Entities.Order?> GetOrderByIdAsync(Guid orderId);
+    Task<Domain.Entities.Order?> GetOrderByIdAsync(Guid orderId,Guid userId);
     Task CreateOrderAsync(Domain.Entities.Order order);
     void UpdateOrder(Domain.Entities.Order order);
     Task SaveAsync();
+    Task<List<Domain.Entities.Order>?> GetAllOrders();
+    Task<List<Domain.Entities.Order>?> GetOrderByUserIdAsync(Guid userId);
 }
